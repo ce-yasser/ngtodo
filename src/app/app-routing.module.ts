937535b3afcda4807a2a2b5fcc31d9f893272005
ahmed yasser <ce.ahmed.yasser@gmail.com>
2022-05-21 11:43:47 +0200

    Remove extra code in appcomponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddTodoComponent } from './pages/add-todo/add-todo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{ path: 'add', component: AddTodoComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
